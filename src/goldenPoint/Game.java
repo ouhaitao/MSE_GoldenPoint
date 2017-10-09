e256590daf95e1a02160fcfa1ae467b73b2cf26c
ouhaitao <oht674213706@vip.qq.com>
2017-10-09 19:37:56 +0800

    test2

--- src/goldenPoint/Game.java
+++ src/goldenPoint/Game.java
@@ -1,8 +1,6 @@
 package goldenPoint;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -13,13 +11,13 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Game
  */
-@WebServlet("/star")
+@WebServlet(urlPatterns = "/star", asyncSupported = true)
 public class Game extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private int numCount = 0;
+	private volatile int numCount = 0;
 	private double[] num = new double[3];
 
-	private volatile Object lock = new Object();
+	// private volatile Object lock = new Object();
 
 	/**
 	 * @see HttpServlet#HttpServlet()
@@ -38,41 +36,36 @@ public class Game extends HttpServlet {
 		
 		if(numCount==3)
 			numCount=0;
-		synchronized (lock) {
-			double t = Double.parseDouble(request.getParameter("number"));
-			num[numCount++]=t;
-			while (numCount != 3) {
-				try {
-					lock.wait();
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
-			double result = 0;
-			for (double i : num) {
-				result += i;
-			}
-			result = result / 3 * 0.618;
-			//第一名的数字
-			double first=200;
-			//最后一名的数字
-			double last=result;
-			for (double i : num) {
-				if(Math.abs(i-result)<Math.abs(first-result)){
-					first=i;
-					continue;
-				}
-				if(Math.abs(i-result)>Math.abs(last-result)){
-					last=i;
-					continue;
-				}
-			}
-			request.setAttribute("result", result);
-			request.setAttribute("first", first);
-			request.setAttribute("last", last);
-			request.getRequestDispatcher("result.jsp").forward(request, response);
-			lock.notifyAll();
+		double t = Double.parseDouble(request.getParameter("number"));
+		num[numCount++] = t;
+		while(numCount!=3){
+			Thread.yield();
 		}
+		request.getSession().setAttribute("num", numCount);
+		double result = 0;
+		for (double i : num) {
+			result += i;
+		}
+
+		result = result / 3 * 0.618;
+		// 第一名的数字
+		double first = 200;
+		// 最后一名的数字
+		double last = result;
+		for (double i : num) {
+			if (Math.abs(i - result) < Math.abs(first - result)) {
+				first = i;
+				continue;
+			}
+			if (Math.abs(i - result) > Math.abs(last - result)) {
+				last = i;
+				continue;
+			}
+		}
+		request.setAttribute("result", result);
+		request.setAttribute("first", first);
+		request.setAttribute("last", last);
+		request.getRequestDispatcher("result.jsp").forward(request, response);
 	}
 
 	/**
